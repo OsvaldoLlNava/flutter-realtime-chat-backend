bd5b2324aee8b42b05e77d0f360aedbd284d5e3e
Osvaldo Ll. Nava <luis_llanesn@hotmail.com>
2021-01-12 13:58:31 -0600

    conectado y desconectado en BD

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,5 +1,6 @@
-const { comprobarJWT } = require('../helpers/jwt');
 const { io } = require('../index');
+const { comprobarJWT } = require('../helpers/jwt');
+const { usuarioConectado, usuarioDesconectado } = require('../controllers/socket');
 
 
 // Mensajes de Sockets
@@ -15,8 +16,13 @@ io.on('connection', client => {
         return client.disconnect();
     }
 
+    //cliente autenticado
+    usuarioConectado(uid);
+
+
     client.on('disconnect', () => {
-        console.log('Cliente desconectado');
+        // console.log('Cliente desconectado');
+        usuarioDesconectado(uid);
     });
 
     // client.on('mensaje', ( payload ) => {
