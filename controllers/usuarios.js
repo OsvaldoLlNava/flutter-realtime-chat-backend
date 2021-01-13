eccdc2a7654d969ec322ff30c86e0b1924466a7f
Osvaldo Ll. Nava <luis_llanesn@hotmail.com>
2021-01-13 15:14:25 -0600

    Terminada la aplicacion del curso
    Se termino la aplicacion del curso y ya esta completo para la
    comunicacion de 1 a 1 en solo texto

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -10,6 +10,8 @@ const getUsuarios = async(req, res = response) => {
     const usuarios = await Usuario
         .find({ _id: { $ne: req.uid } })
         .sort('-online')
+        .skip(desde)
+        .limit(30);
 
 
 
